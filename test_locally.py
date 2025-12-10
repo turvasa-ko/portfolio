c1801253486bf6625c2f4adc9ac44f0ff9b03c97
Tatu Tallavaara <ttallava19@student.oulu.fi>
2025-12-10 23:23:02 +0200

    CSS file started properly

--- test_locally.py
+++ test_locally.py
@@ -5,8 +5,6 @@ import subprocess
 PORT = 9000
 Handler = http.server.SimpleHTTPRequestHandler
 
-subprocess.run(["python", "-m", "http.server", "9000"])
-
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Serving at port {PORT}")
     httpd.serve_forever()
